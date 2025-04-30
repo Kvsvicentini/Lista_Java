65f768a45ee9c085e8c0dff652f4cb143ab4a8a1
Kvsvicentini <kaua.vicentini@gmail.com>
2025-04-30 13:33:06 -0300

    Finalizando a lista 1 em Java

---
+++ Exercicio10.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercicio10 {
+    public static void main(String[] args) throws Exception{
+        double num_1, num_2, num_3, calc_1, calc_2, calc_3;
+        Scanner grava = new Scanner(System.in);
+
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("Vamos ler um número realizar e operações matemáticas");
+
+        System.out.print("Digite um número: ");
+        num_1 = grava.nextDouble();
+
+        calc_1 = num_1 / 2;
+        num_2 = calc_1;
+        num_3 = calc_1;
+        calc_2 = num_2 + 3;
+        calc_3 = num_3 + 5;
+
+        System.out.println("O resultado da primeira operação é " + calc_2 + " e a segunda operação deu " + calc_3);
+
+    }
+}
